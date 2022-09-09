ecaaffd026602cfe3d3f4466966f2479289e43bc
Nico Hämäläinen <nico@vaino.lol>
2022-09-10 00:06:19 +0300

    Added basic server boilerplate

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Snippetbox
 
 A basic pastebin for the purpose of learning the Go programming language.
+
+# Running
+
+```
+$ go run .
+```
+
+# Routes
+
+```
+/               index            Display the home page
+/snippet/view   snippetView      Display a specific snippet
+/snippet/new    snippetCreate    Create a new snippet     
+```
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// The address to bind the snippetbox srver to listen to.
+// Generally you don't need to specify a host in the address unless your
+// compter has multiple network interfaces and you just want to listen
+// on one of them.
+// NOTE: Should follow format "host:port".
+const BindAddress = ":4000"
+
+// home writes a byte slice containing a greeting as the response body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Initialize a new servemux (aka router), then register the home
+	// function as the handler for the index page
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// STart a new web server with the given network address to listen
+	// on and the servemux we just created. If http.ListenAndServe()
+	// returns an error, we use the log.Fatal() function to log the
+	// error message and exit. Note that any error returned by
+	// http.ListenAndServe() is always non-nil.
+	log.Println("Snippetbox server listening on " + BindAddress)
+	err := http.ListenAndServe(BindAddress, mux)
+	log.Fatal(err)
 }
